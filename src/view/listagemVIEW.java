d63dc99e8ae8eb6418571db78ae45b0e6e8fec5b
AndrewGct <niceandrewgct@gmail.com>
2025-08-03 02:53:58 -0300

    Botão Vender ativado na listagemVIEW

--- src/view/listagemVIEW.java
+++ src/view/listagemVIEW.java
@@ -141,12 +141,14 @@ public class listagemVIEW extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
-        String id = id_produto_venda.getText();
-        
-        ProdutosDAO produtosdao = new ProdutosDAO();
-        
-        //produtosdao.venderProduto(Integer.parseInt(id));
-        listarProdutos();
+      try {
+        int id = Integer.parseInt(id_produto_venda.getText());
+        ProdutosDAO dao = new ProdutosDAO();
+        dao.venderProduto(id);
+        listarProdutos(); // Atualiza a tabela após venda
+    } catch (NumberFormatException e) {
+        JOptionPane.showMessageDialog(null, "ID inválido.");
+    }
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
